5be0a8cbb87298650e759437fc65e3361365b7b1
SergioCalderonR <sercal@outlook.com>
2017-09-24 19:44:41 -0500

    GetSystemDefaultLocaleName function

--- Renadmin/Renadmin/Source.c
+++ Renadmin/Renadmin/Source.c
@@ -1,11 +1,33 @@
 #include <Windows.h>
+#include <tchar.h>
 #include <LM.h>
 
 #pragma comment(lib, "Netapi32.lib")
 
+
+
 int wmain(int argc, WCHAR *argv[])
 {
+	//NetUserSetInfo
+	DWORD level = 0;	//USER_INFO_0 structure
+	USER_INFO_0 usrInfo0;
+	DWORD parm_err;
 
+	//GetSystemDefaultLocaleName
+	WCHAR pLocaleName[LOCALE_NAME_MAX_LENGTH];
+	int sizeOfLocale = LOCALE_NAME_MAX_LENGTH;
+
+	if (GetSystemDefaultLocaleName(pLocaleName, sizeOfLocale) != 0)
+	{
+		wprintf(L"\nCurrent Locale: %s\n", pLocaleName);
+	}
+	else
+	{
+		wprintf(L"\nError code: %lu\n", GetLastError());
+
+	}
+
+	getchar();
 
 	return 0;
 }
