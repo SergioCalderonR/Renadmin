afe4ec5498a5e5d465321130df555369b024eebb
SergioCalderonR <sercal@outlook.com>
2017-09-24 20:48:47 -0500

    Error handling

--- Renadmin/Renadmin/Source.c
+++ Renadmin/Renadmin/Source.c
@@ -4,9 +4,31 @@
 
 #pragma comment(lib, "Netapi32.lib")
 
+VOID ShowError(DWORD errCode)
+{
+	DWORD flags = FORMAT_MESSAGE_ALLOCATE_BUFFER |
+				FORMAT_MESSAGE_FROM_SYSTEM |
+				FORMAT_MESSAGE_IGNORE_INSERTS;
+
+	DWORD langId = LANG_USER_DEFAULT;
+	LPWSTR errMsg;
+
+	if (FormatMessageW(flags, NULL, errCode, langId, (LPWSTR)&errMsg, 0, NULL) == 0)
+	{
+		wprintf(L"Error code: %lu\n", GetLastError());
+
+	}
+
+	wprintf(L"\n%s\n", errMsg);
+
+	LocalFree(errMsg);
+
+}
+
 int wmain(int argc, WCHAR *argv[])
 {
 	//NetUserSetInfo
+	NET_API_STATUS netUserSet;
 	DWORD level = 0;	//USER_INFO_0 structure
 	USER_INFO_0 usrInfo0 = {L"Administrator"};
 	DWORD parm_err;
@@ -15,29 +37,37 @@ int wmain(int argc, WCHAR *argv[])
 	WCHAR pLocaleName[LOCALE_NAME_MAX_LENGTH];
 	int sizeOfLocale = LOCALE_NAME_MAX_LENGTH;
 
+	if (argc == 1)
+	{
+		fwprintf(stderr, L"Usage: %s NewAdminName", argv[0]);
+		exit(1);
+	}
+
 	if (GetUserDefaultLocaleName(pLocaleName, sizeOfLocale) != 0)
 	{
 		if (_wcsicmp(pLocaleName, L"es-ES") == 0 ||
-			_wcsicmp(pLocaleName, L"es-CO") == 0)
+			_wcsicmp(pLocaleName, L"es-CO") == 0 ||
+			_wcsicmp(pLocaleName, L"es-EC") == 0 ||
+			_wcsicmp(pLocaleName, L"es-VE") == 0)			
 		{
-			wprintf(L"%s", pLocaleName);
+			wprintf(L"\nCurrent Language ID= %s", pLocaleName);
 
-			if (NetUserSetInfo(NULL, L"Administrador", level, (LPBYTE)&usrInfo0, &parm_err) != NERR_Success)
+			netUserSet = NetUserSetInfo(NULL, argv[1], level, (LPBYTE)&usrInfo0, &parm_err);
+
+			if (netUserSet != NERR_Success)
 			{
-				wprintf(L"Error code: %lu\n", GetLastError());
+				ShowError(netUserSet);
 			}
 			else
 			{
 				wprintf(L"\nBuilt-in admin account name has been change to Administrator\n");
 			}
 
-		}
-		
-		
+		}		
 	}
 	else
 	{
-		wprintf(L"\nError code: %lu\n", GetLastError());
+		ShowError(GetLastError());
 
 	}
 	
